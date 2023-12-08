2d5f1860152f16798652fe88ac8c2b62919d1236
Godzilla810 <128792917+Godzilla810@users.noreply.github.com>
2023-12-08 20:14:50 +0800

    Add Button art

--- Assets/Scripts/Control/UIControllerForStartScene.cs
+++ Assets/Scripts/Control/UIControllerForStartScene.cs
@@ -6,15 +6,17 @@ public class UIControllerForStartScene : MonoBehaviour
 {
     public GameObject startBtn;
     public GameObject information;
+    public GameObject character;
     public void ShowInformation()
     {
         startBtn.SetActive(false);
         information.SetActive(true);
-
+        character.SetActive(false);
     }
     public void HideInformation()
     {
         startBtn.SetActive(true);
         information.SetActive(false);
+        character.SetActive(true);
     }
 }
